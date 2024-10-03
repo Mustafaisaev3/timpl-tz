fff294c31b7d810a95059563945e691442b454e9
Mustafa Isaev <mustafaisaev3@gmail.com>
2024-10-03 07:19:21 +0300

    Making Rating component

---
+++ src/components/shared/Rating.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="rating-component">
+    <div class="rating-component__header">
+      <div class="rating-component__line"></div>
+      <h1 class="rating-component__title">{{ title }}</h1>
+      <div class="rating-component__line"></div>
+    </div>
+    <div class="rating-component__stars">
+      <img
+        v-for="(star, index) in stars"
+        :key="index"
+        :src="star ? activeStar : inactiveStar"
+        @click="setRating(index + 1)"
+        class="rating-component__star"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const activeStar = require('@/assets/images/icons/active-star.svg');
+const inactiveStar = require('@/assets/images/icons/inactive-star.svg');
+
+interface RatingProps {
+  title: string;
+}
+
+const { title } = defineProps<RatingProps>();
+
+const stars = ref<boolean[]>([false, false, false, false, false]);
+
+const setRating = (rating: number) => {
+  stars.value = stars.value.map((_, index) => index < rating);
+};
+</script>
+
+<style lang="scss" scoped>
+.rating-component {
+  text-align: center;
+  margin-inline: 20px;
+
+  &__header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 12px 0px;
+  }
+
+  &__line {
+    width: 100%;
+    height: 1px;
+    background-color: #9771FD;
+    margin: 0 10px;
+  }
+
+  &__title {
+    font-size: 15px;
+    white-space: nowrap;
+    margin: 0;
+  }
+
+  &__stars {
+    display: flex;
+    justify-content: center;
+    padding: 16px 0px;
+  }
+
+  &__star {
+    cursor: pointer;
+    margin: 0 5px;
+  }
+}
+</style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@
     <Marquee :texts="['Part of the profit goes to sWen', 'By leaving a tip, you take care of nature', 'Part of the profit goes to sWen', 'By leaving a tip, you take care of nature']" />
     <UserProfile />
     <Tips />
+    <Rating :title="'Rate your experience'" />
   </div>
 </template>
 
@@ -12,4 +13,5 @@ import Header from '@/components/Header.vue';
 import Marquee from '@/components/shared/Marquee.vue';
 import UserProfile from '@/components/shared/UserProfile.vue';
 import Tips from '@/components/shared/Tips.vue';
+import Rating from '@/components/shared/Rating.vue';
 </script>
