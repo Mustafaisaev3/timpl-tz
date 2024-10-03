ff0befd7b749ace6f9de6aae7ad2436885f3990a
Mustafa Isaev <mustafaisaev3@gmail.com>
2024-10-03 06:05:36 +0300

    Making UserProfile component

---
+++ src/components/shared/UserProfile.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__side user-profile__side--left">
+      <img src="@/assets/images/user-left.svg" alt="Side Image Left" class="user-profile__side-image" />
+    </div>
+    <div class="user-profile__main">
+      <img src="@/assets/images/user-main.svg" alt="Main Image" class="user-profile__main-image" />
+      <div class="user-profile__main-info">
+        <h2 class="user-profile__main-name">Madison</h2>
+        <h2 class="user-profile__main-surname">Waiter</h2>
+      </div>
+    </div>
+    <div class="user-profile__side user-profile__side--right">
+      <img src="@/assets/images/user-right.svg" alt="Side Image Right" class="user-profile__side-image" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+.user-profile {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 16px;
+  margin-bottom: 20px;
+
+  &__side {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+
+    &--left {
+      justify-content: flex-end;
+    }
+
+    &--right {
+      justify-content: flex-start;
+    }
+
+    &-image {
+      width: 100%;
+      height: 66px;
+    }
+  }
+
+  &__main {
+    flex: 2;
+    display: flex;
+    align-items: center;
+    gap: 12px;
+
+    &-image {
+      width: 92px;
+      height: 92px;
+      border-radius: 50%;
+    }
+
+    &-info {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: baseline;
+    }
+
+    &-name {
+      color: #0F0A2F;
+      font-size: 22px;
+      font-weight: 600;
+    }
+    
+    &-surname {
+      color: #9771FD;
+      font-size: 16px;
+      font-weight: 600;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,12 @@
   <div class="home">
     <Header />
     <Marquee :texts="['Part of the profit goes to sWen', 'By leaving a tip, you take care of nature', 'Part of the profit goes to sWen', 'By leaving a tip, you take care of nature']" />
+    <UserProfile />
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from '@/components/Header.vue';
 import Marquee from '@/components/shared/Marquee.vue';
+import UserProfile from '@/components/shared/UserProfile.vue';
 </script>
